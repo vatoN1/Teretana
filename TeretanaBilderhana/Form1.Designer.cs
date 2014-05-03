66b43fc5845106fe4a6b084c0a128c3222455054
Aldin Kiselica <aldin.kiselica.94@gmail.com>
2014-05-03 17:12:46 +0200

    test

--- TeretanaBilderhana/Form1.Designer.cs
+++ TeretanaBilderhana/Form1.Designer.cs
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Bilderhana v1.2";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
