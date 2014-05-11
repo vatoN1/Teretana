e946306ce2e320b6e24399c5e29e600244da6d85
Aldin Kiselica <aldin.kiselica.94@gmail.com>
2014-05-11 21:59:46 +0200

    gui login

--- TeretanaBilderhana/Form1.cs
+++ TeretanaBilderhana/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.ExceptionServices;
 
 namespace TeretanaBilderhana
 {
@@ -15,6 +16,61 @@ namespace TeretanaBilderhana
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int triger = 1;
+            try
+            {
+                if (usertext.Text == "teretana" && passtext.Text == "bilderhana")
+                {
+                    Form2 f = new Form2();
+                    f.Show();
+                }
+                else if (usertext.Text == "teretana" && passtext.Text != "bilderhana")
+                {
+                    errorProvider1.SetError(usertext, "");
+                    errorProvider1.SetError(passtext, "Pogresna sifra");
+                }
+                else if (usertext.Text != "teretana" && passtext.Text == "bilderhana")
+                {
+                    errorProvider1.SetError(usertext, "Pogresan korisnicki nalog");
+                    errorProvider1.SetError(passtext, "");
+                }
+                else
+                {
+
+                    bool flag = false;
+                    for (int i = 0; i < passtext.Text.Length; i++)
+                    {
+                        char x = Convert.ToChar(passtext.Text[i]);
+                        if (!IsLetterOrDigit(x))
+                        {
+                            flag = true;
+                        }
+                    }
+                    baciIzuzetak(flag);
+
+                    errorProvider1.SetError(usertext, "Pogresan korisnicki nalog");
+                    errorProvider1.SetError(passtext, "Pogresna sifra");
+                }
+            }
+            catch (Exception)
+            {
+            }
+       
+        }
+
+        bool IsLetterOrDigit(char c) { return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'); }
+        private void baciIzuzetak(bool flag)
+        {
+            if (flag == true) throw new Exception("HAJD BAR NEŠTO IZBACI");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
